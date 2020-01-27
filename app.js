1b13bf561f4d7cd57fe0c4eaf568d22a8b2a1c35
Shivansh2407 <shivanshsrivastava2000@gmail.com>
2020-01-27 15:59:28 +0530

    Setting Up POST Model.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -17,4 +18,10 @@ app.use((req, res,  next) => {
 
 app.use('/feed', feedRoutes);
 
+mongoose.connect(
+    'mongodb+srv://Octrix:Octrix@cluster0-5p2x1.gcp.mongodb.net/test?retryWrites=true&w=majority'
+).then(result => {
+    app.listen(8001);
+})
+.catch(err => console.log(err));
 app.listen(8000);
