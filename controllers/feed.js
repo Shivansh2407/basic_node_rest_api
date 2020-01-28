0695b13965af557c2c73f32e0f5d2e690758248b
Shivansh2407 <shivanshsrivastava2000@gmail.com>
2020-01-28 18:47:13 +0530

    Uploading Images

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,19 +3,18 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post')
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts:[
-            {
-                _id: '1',
-                title:'First Post',
-                content: 'This is the first post!', 
-                imageUrl: 'images/pup.jpeg',
-                creator:{
-                    name: 'Maximilian'
-                },
-            createdAt: new date()
-            }
-        ]
+    Post.find()
+    .then(posts => {
+        res.status(200).json({
+            message:'Fetched Posts Successfully',
+            posts:posts
+        });
+    })
+    .catch(err =>{
+        if(!err.statusCode){
+            err.statusCode = 500 ;
+        }
+        next(err);
     });
 };
 exports.createPost = (req, res, next) => {
@@ -25,12 +24,18 @@ exports.createPost = (req, res, next) => {
         error.statusCode = 422;
         throw error;
     }
+    if(!req.file){
+        const error = new Error('No Image Provided.');
+        error.statusCode = 422 ; 
+        throw error;
+    }
+    const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
     const post = new Post({
             title: title, 
             content: content,
-            imageUrl: 'images/pup.jpeg',
+            imageUrl: imageUrl,
             creator: { name: 'Maximilian' }
             
     });
