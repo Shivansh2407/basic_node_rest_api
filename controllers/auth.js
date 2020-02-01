e2086eea81548ea2564d6d71367723174f740ab1
Shivansh2407 <shivanshsrivastava2000@gmail.com>
2020-02-01 09:54:27 +0530

    Starting with User Login.

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,3 +32,32 @@ exports.signup = (req, res, next) => {
         next(err);        
     });
 };
+
+exports.login = (req, res, next) =>{
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadUser;
+    User.findOne({email : email})
+        .then(user => {
+            if(!user){
+                const error = new Error('User with this email could not be fouud')
+                error.statusCode = 401;
+                throw error;
+            }
+            loadUser = user;
+            return bcrypt.compare(password, user.password)
+        })
+        .then(isEqual => {
+            if(!isEqual) {
+                const error = new Error('Wrong Password!');
+                error.statusCode = 401;
+                throw error;
+            }
+        })
+        .catch(err =>{
+            if(!err.statusCode){
+                err.statusCode = 500 ;
+            }
+            next(err);   
+        });
+};
