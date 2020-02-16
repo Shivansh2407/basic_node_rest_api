55e19021e1d9c600cb1adce225603eb8f2158800
Shivansh2407 <shivanshsrivastava2000@gmail.com>
2020-02-16 21:00:55 +0530

    Clear Post-User Relations.

--- controllers/feed.js
+++ controllers/feed.js
@@ -169,7 +169,13 @@ exports.deletePost = (req, res, next) => {
             return Post.findByIdAndRemove(postId);
         })
         .then(reult => {
-            console.log(result);
+                return User.findById(req.userId);
+         })
+        .then(result => {
+            user.posts.pull(postId);
+            return user.save();
+        })
+        .then(result => {
             res.status(200).json({message: 'Deleted Post.'});
         })
         .catch(err =>{
